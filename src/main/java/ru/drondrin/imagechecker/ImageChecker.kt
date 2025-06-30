b64901cad776f0691fa7a37f2195ea357336249c
DronDrin <FunnyFunC@yandex.ru>
2025-06-30 15:25:12 +0300

    Refactor code

--- src/main/java/ru/drondrin/imagechecker/ImageChecker.kt
+++ src/main/java/ru/drondrin/imagechecker/ImageChecker.kt
@@ -1,15 +1,14 @@
 package ru.drondrin.imagechecker
 
 import com.drew.imaging.ImageMetadataReader.readMetadata
-import com.drew.metadata.Tag
 import com.drew.metadata.file.FileTypeDirectory
 import java.io.File
 
 fun main() {
     print("Enter path: ")
-    File(readln()).walk().toList().filter {
-            it.extension in listOf("png", "jpg") && it.extension !in readMetadata(it)
-                .getFirstDirectoryOfType(FileTypeDirectory::class.java).tags.map(Tag::getDescription)
-        }.apply { println("Image files with wrong extension: ${(if (isEmpty()) "\n(none)" else "")}") }
-            .forEach { println(it.name) }
+    File(readln()).walk().toList().filter { f ->
+        f.extension in listOf("png", "jpg") && f.extension !in readMetadata(f)
+            .getFirstDirectoryOfType(FileTypeDirectory::class.java).tags.map { it.description }
+    }.map { it.name }.apply { println("Image files with wrong extension: ${(if (isEmpty()) "\n(none)" else "")}") }
+        .forEach(::println)
 }

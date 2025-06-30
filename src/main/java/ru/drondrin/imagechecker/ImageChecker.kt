4bb1beb719c6e298b14a26c7c3818383a9efa7cc
DronDrin <FunnyFunC@yandex.ru>
2025-06-30 15:00:35 +0300

    Rewrite in kotlin

--- src/main/java/ru/drondrin/imagechecker/ImageChecker.kt
+++ src/main/java/ru/drondrin/imagechecker/ImageChecker.kt
@@ -1,41 +1,14 @@
-package ru.drondrin.imagechecker;
+package ru.drondrin.imagechecker
 
-import com.drew.imaging.ImageMetadataReader;
-import com.drew.imaging.ImageProcessingException;
-import com.drew.metadata.Tag;
-import com.drew.metadata.file.FileTypeDirectory;
+import com.drew.imaging.ImageMetadataReader.readMetadata
+import com.drew.metadata.Tag
+import com.drew.metadata.file.FileTypeDirectory
+import java.io.File
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Scanner;
-
-public class ImageChecker {
-    public static void main(String[] args) throws IOException, ImageProcessingException {
-        var scanner = new Scanner(System.in);
-        System.out.print("Enter path: ");
-        String rootPath = scanner.nextLine();
-
-        rootPath = rootPath.replaceFirst("\\\\$", "");
-
-        System.out.println("Image files with wrong extension:");
-        boolean foundWrongFiles = false;
-        try (var walkStream = Files.walk(Path.of(rootPath))) {
-            for (var file : walkStream.map(Path::toFile).toList()) {
-                String[] nameSplit = file.getName().split("\\.");
-                String extension = nameSplit[nameSplit.length - 1];
-                if ((extension.equals("jpg") || extension.equals("png")) &&
-                        ImageMetadataReader.readMetadata(file)
-                                .getFirstDirectoryOfType(FileTypeDirectory.class)
-                                .getTags().stream().map(Tag::getDescription).map(String::toLowerCase)
-                                .noneMatch(t -> t.equals(extension.toLowerCase()))) {
-                    System.out.println(file.getAbsolutePath().replace(rootPath + "\\", ""));
-                    foundWrongFiles = true;
-                }
-            }
-        }
-        if (!foundWrongFiles) {
-            System.out.println("(none)");
-        }
-    }
+fun main() {
+    print("Enter path: ")
+    File(readLine()!!).walk().toList().filter {
+        it.extension in listOf("png", "jpg") && it.extension !in readMetadata(it)
+            .getFirstDirectoryOfType(FileTypeDirectory::class.java).tags.map(Tag::getDescription)
+    }.apply { println("Image files with wrong extension:" + (if (isEmpty()) "\n(none)" else "")) }.forEach(::println)
 }
